2e2882d41700d7479db66359bd2c90d67ee9dc20
karalundi2019 <46692928+karalundi2019@users.noreply.github.com>
2019-01-14 16:19:56 -0600

    Removed numberOfImagesTaken
    Removed numberOfImagesTaken for no display on the capture icon.

--- src/CameraScreen/CameraKitCameraScreenBase.js
+++ src/CameraScreen/CameraKitCameraScreenBase.js
@@ -170,17 +170,6 @@ export default class CameraScreenBase extends Component {
     );
   }
 
-  numberOfImagesTaken() {
-    const numberTook = this.state.captureImages.length;
-    if (numberTook >= 2) {
-      return numberTook;
-    } else if (this.state.captured) {
-      return '1';
-    } else {
-      return '';
-    }
-  }
-
   renderCaptureButton() {
     return (this.props.captureButtonImage && !this.isCaptureRetakeMode()) &&
       <View style={styles.captureButtonContainer}>
